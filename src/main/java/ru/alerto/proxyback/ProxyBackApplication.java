6c179a48c097df1e795f31a76f1d6bcb2b694d00
alerto <alexbut006@gmail.com>
2025-03-12 08:11:30 +0300

    Первая загрузка

---
+++ src/main/java/ru/alerto/proxyback/ProxyBackApplication.java
@@ -0,0 +1,13 @@
+package ru.alerto.proxyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyBackApplication.class, args);
+    }
+
+}
